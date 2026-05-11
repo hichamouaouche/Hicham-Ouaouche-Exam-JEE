d642f24d75c45f521168c94c3fcf2818acf11ae9
hichamouaouche <hichamouaouche@gmail.com>
2026-05-11 14:08:32 +0100

    first

---
+++ backend-hicham-ouaouche/src/main/java/org/sid/backendhichamouaouche/BackendHichamOuaoucheApplication.java
@@ -0,0 +1,13 @@
+package org.sid.backendhichamouaouche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHichamOuaoucheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHichamOuaoucheApplication.class, args);
+    }
+
+}
